9516739158598f9729a582691d53bae696a6e2b8
Elias Steurer <eliasjoaquinsteurer@gmail.com>
2021-10-09 17:15:08 -0300

    arreglos

--- pages-of-product/enviar.php
+++ pages-of-product/enviar.php
@@ -1,15 +1,15 @@
 <?php
     $destino ="eliasjoaquinsteurer@gmail.com";
-    $nombre = $_POST["Nombre"];
-    $apellido = $_POST["Apellido"];
-    $email = $_POST["Email"];
-    $país = $_POST["País"];
-    $calle = $_POST["Dirección de calle"];
-    $ciudad = $_POST["Localidad"];
-    $provincia = $_POST["Provincia"];
-    $postal = $_POST["Código postal"];
-    $telefono = $_POST["Teléfono"];
-    $producto = $_POST["Especificaion de producto"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $email = $_POST["email"];
+    $país = $_POST["pais"];
+    $calle = $_POST["dirección-de-calle"];
+    $ciudad = $_POST["localidad"];
+    $provincia = $_POST["provincia"];
+    $postal = $_POST["codigo-postal"];
+    $telefono = $_POST["telefono"];
+    $producto = $_POST["especificaion-de-producto"];
     $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nEmail: " . $email . "\nPaís: " . $país . "\nDirección de la calle: " . $calle . "\nLocalidad / Ciudad: " . $ciudad . "\nProvincia / Región: " . $provincia . "\nCódigo postal: " . $postal . "\nTeléfono: " . $telefono . "\nProducto en especifico: " . $producto;
     mail($destino, "Pedido", $contenido);
     header("Location: gracias.html");
